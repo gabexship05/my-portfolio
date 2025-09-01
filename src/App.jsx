b6f6ede4a9168212f0575a66f437e30c969273dd
Gabriel Perez <gabolion06@gmail.com>
2025-08-31 20:46:00 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500 text-white text-3xl font-bold">
+      Hello, Gabriel! Tailwind is working 🎉
+    </div>
+  );
+}
